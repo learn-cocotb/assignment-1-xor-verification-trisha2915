a0ebf8b79b84b5a88b55daf41a48ab37f94f5598
trisha2915 <86705840+trisha2915@users.noreply.github.com>
2023-01-15 17:44:35 +0530

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,7 +1,16 @@
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.triggers import Timer, RisingEdge
 
 
 @cocotb.test()
 async def dut_test(dut):
-    assert 0, "Test Not Implemented Error"
+    
+    a = (0, 0, 1, 1);
+    b = (0, 1, 0, 1);
+    y = (0, 1, 1, 0);
+    
+    for i in range(4):
+       dut.a.value = a[i]
+       dut.b.value = b[i]
+       await Timer(1, 'ns')
+       assert dut.y.value == y[i], f"Error at iteration"
