53f335b2c8d00715eb530dcdaae91e1e6a6de93c
trisha2915 <86705840+trisha2915@users.noreply.github.com>
2023-01-19 15:18:30 +0530

    Update xor_test.py

--- tests/xor_test.py
+++ tests/xor_test.py
@@ -3,14 +3,14 @@ from cocotb.triggers import Timer, RisingEdge
 
 
 @cocotb.test()
-async def xor_test(dut):
+async def xor_test(xor_gate):
     
     a = (0, 0, 1, 1);
     b = (0, 1, 0, 1);
     y = (0, 1, 1, 0);
     
     for i in range(4):
-       dut.a.value = a[i]
-       dut.b.value = b[i]
+       xor_gate.a.value = a[i]
+       xor_gate.b.value = b[i]
        await Timer(1, 'ns')
-       assert dut.y.value == y[i], f"Error at iteration"
+       assert xor_gate.y.value == y[i], f"Error at iteration"
